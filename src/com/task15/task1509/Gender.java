d2ab32e554c9c51a3ad468873317600b58532e8f
Sergii Kroshka <SKroshka@luxoft.com>
2018-04-24 18:41:48 +0300

    task15 define gender of human and print it's classname

---
+++ src/com/task15/task1509/Gender.java
@@ -0,0 +1,31 @@
+package com.task15.task1509;
+
+public class Gender {
+
+	public static void main(String[] args) {
+
+		Human man = new Man();
+		Human woman = new Woman();
+
+		printName(man);
+		printName(woman);
+	}
+
+	public static void printName(Human man) {
+		System.out.println(man.getClass().getSimpleName());
+
+	}
+
+	public static class Human {
+
+	}
+
+	public static class Man extends Human {
+
+	}
+
+	public static class Woman extends Human {
+
+	}
+
+}
