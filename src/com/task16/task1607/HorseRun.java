11108c00a16a1c4bbd28a71f82ea0dca8b5c790e
Sergii Kroshka <pm012@ukr.net>
2018-06-05 18:45:43 +0300

    HorseRun Threads task

---
+++ src/com/task16/task1607/HorseRun.java
@@ -0,0 +1,73 @@
+package com.task16.task1607;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HorseRun {
+
+	public static int countHorses = 10;
+
+	public static void main(String[] args) throws InterruptedException {
+		List<Horse> horses = prepareHorsesAndStart();
+		int num = 0;
+		while ((num = calculateHorsesFinished(horses)) != countHorses) {
+		}
+		System.out.println(num);
+	}
+
+	public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {
+		int countFinished = 0;
+		// add your implementation here
+		for (Horse horse : horses) {
+			if (horse.isFinished) {
+				countFinished++;
+			} else {
+				System.out.println("Waiting for " + horse.getName());
+				horse.join();
+			}
+		}
+
+		return countFinished;
+	}
+
+	public static List<Horse> prepareHorsesAndStart() {
+		List<Horse> horses = new ArrayList<Horse>(countHorses);
+		String number;
+		for (int i = 1; i < countHorses + 1; i++) {
+			number = i < 10 ? ("0" + i) : "" + i;
+			horses.add(new Horse("Horse_" + number));
+		}
+
+		for (int i = 0; i < countHorses; i++) {
+			horses.get(i).start();
+		}
+		return horses;
+	}
+
+	public static class Horse extends Thread {
+
+		private boolean isFinished;
+
+		public Horse(String name) {
+			super(name);
+		}
+
+		public boolean isFinished() {
+			return isFinished;
+		}
+
+		@Override
+		public void run() {
+			String s = "";
+			for (int i = 0; i < 1001; i++) { // delay
+				s += "" + i;
+				if (i == 1000) {
+					s = " has finished the race!";
+					System.out.println(getName() + s);
+					isFinished = true;
+				}
+			}
+		}
+
+	}
+}
