c65182fe7b65e0769de7805451b287f3b1c1d5ef
Sergii Kroshka <SKroshka@luxoft.com>
2018-06-13 13:38:27 +0300

    Added thread and join Added file for testing

--- src/com/javarush/task16/task1630/InputOutputFilesInThreads.java
+++ src/com/javarush/task16/task1630/InputOutputFilesInThreads.java
@@ -1,13 +1,15 @@
 package com.javarush.task16.task1630;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /*
- * 1. Sort it out what the program is doing
- * 2. In static block read 2 files firstFileName and secondFileName.
- * 3. Inside the Solution class create thread public static ReadFileThread, that implements interface ReadFileInterface ( Think what fits best: Thread or Runnable)
+ * 1. Sort it out what the program is doing +
+ * 2. In static block read 2 files firstFileName and secondFileName. +
+ * 3. Inside the Solution class create thread public static ReadFileThread, that implements interface ReadFileInterface ( Think what fits best: Thread or Runnable) +
  * 3.1 Method setFileName should define the name of the file and  read its content
  * 3.2 Method getFileContent should return file content
  * 3.3 Read all contents of the file and close thread. Split with whitespace the rows of the file.
@@ -19,7 +21,7 @@ public class InputOutputFilesInThreads {
 	public static String firstFileName;
 	public static String secondFileName;
 
-	// add your code here - добавьте код тут
+	// add your code here
 
 	static {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -47,7 +49,8 @@ public class InputOutputFilesInThreads {
 		ReadFileInterface f = new ReadFileThread();
 		f.setFileName(fileName);
 		f.start();
-		// add your code here - добавьте код тут
+		f.join();
+		// add your code here +
 		System.out.println(f.getFileContent());
 	}
 
@@ -62,37 +65,37 @@ public class InputOutputFilesInThreads {
 		public void start();
 	}
 
-	// add your code here - добавьте код тут
+	// add your code here +
+	// we need to extend class Thread because we have tha same unimplemented methods
+	// in class Runnable as in ReadFileInterface - and that's the problem
 
-	public static class ReadFileThread implements ReadFileInterface, Runnable {
+	public static class ReadFileThread extends Thread implements ReadFileInterface {
+		StringBuffer sb = new StringBuffer();
 
 		@Override
 		public void setFileName(String fullFileName) {
-			// TODO Auto-generated method stub
+			super.setName(fullFileName);
 
 		}
 
 		@Override
 		public String getFileContent() {
-			// TODO Auto-generated method stub
-			return null;
-		}
 
-		@Override
-		public void join() throws InterruptedException {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void start() {
-			// TODO Auto-generated method stub
-
+			return sb.toString();
 		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
+			try {
+				BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(getName())));
+				while (br.ready())
+					sb.append(br.readLine() + " ");
+				br.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
 
 		}
 
